8aef954e423ccb72486f50289c1e890bf14f72a7
usuario <salomao@taimber.com>
2022-02-28 14:50:58 -0300

    Primeira estrutura construida

---
+++ src/main/java/br/com/salomaotech/FinancasPessoaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.salomaotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasPessoaisApplication.class, args);
+	}
+
+}
